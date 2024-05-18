cb529740830db5dee11769c607207700c12f4d60
vasanthkorada <vasanthkorada999@gmail.com>
2024-05-19 04:31:38 +0530

    Added JIRA Ticket Creation

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -12,6 +12,9 @@ import 'package:path_provider/path_provider.dart';
 import 'package:uuid/uuid.dart';
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:http/http.dart' as http;
+import 'package:mailer/mailer.dart';
+import 'package:mailer/smtp_server.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class Dashboard extends StatefulWidget {
   const Dashboard({super.key});
@@ -22,6 +25,7 @@ class Dashboard extends StatefulWidget {
 
 class _DashboardState extends State<Dashboard> {
   final ScreenshotController screenshotController = ScreenshotController();
+  bool loading = false;
 
   Future<void> takeScreenshotAndUpload({required String errorMessage, required StackTrace stackTrace}) async {
     try {
@@ -144,6 +148,7 @@ class _DashboardState extends State<Dashboard> {
     if (response.statusCode == 201) {
       print('Issue created successfully');
       print('Response body: ${response.body}');
+      await sendEmail(jiraUrl: "https://infytech.atlassian.net/jira/software/projects/KAN/boards/1?selectedIssue=${jsonDecode(response.body)["key"]}");
     } else {
       print('Failed to create issue');
       print('Status code: ${response.statusCode}');
@@ -151,120 +156,147 @@ class _DashboardState extends State<Dashboard> {
     }
   }
 
+
+  Future<void> sendEmail({required String jiraUrl}) async {
+    final smtpServer = gmail('infytech2019@gmail.com', 'sqms snnk kykp ycuw');
+
+    final message = Message()
+      ..from = const Address('infytech2019@gmail.com', 'DevGuard AI')
+      ..recipients.add('vasanthkorada999@gmail.com')
+      ..subject = 'Priority: High - JIRA ISSUE ASSIGNED'
+      ..text = 'Please click the below JIRA link to know more about the high priority bug $jiraUrl'
+    ..html = '<a href="$jiraUrl">JIRA ISSUE LINK</a>';
+
+    try {
+      final sendReport = await send(message, smtpServer);
+      print('Message sent: $sendReport');
+    } catch (e) {
+      print('Error occurred while sending email: $e');
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Screenshot(
       controller: screenshotController,
-      child: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          backgroundColor: Colors.pink,
-          title: const Text(
-            "SHOP INFY",
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.w700,
-            ),
-          ),
-        ),
-        drawer: Drawer(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              const UserAccountsDrawerHeader(
-                accountName: Text("Hello, Priyanka"),
-                accountEmail: Text("angelme@gmail.com"),
-                currentAccountPicture: CircleAvatar(
-                  backgroundImage: NetworkImage(
-                      "https://media.licdn.com/dms/image/D5635AQE2mVueX0wykA/profile-framedphoto-shrink_800_800/0/1713593490025?e=1716390000&v=beta&t=Rm9vFRZK4FGi9X56MOqlAlKK5e12isIRd-zvPeQ9zPk"),
-                ),
-              ),
-              ListTile(
-                onTap: () {
-                  Navigator.of(context).push(
-                    CupertinoPageRoute(
-                      builder: (context) => const SimulationScreen(),
-                    ),
-                  );
-                },
-                leading: const Icon(Icons.accessibility),
-                title: const Text(
-                  "Simulation",
-                ),
-              )
-            ],
-          ),
-        ),
-        bottomNavigationBar: SizedBox(
-          height: 66,
-          child: OutlinedButton(
-            style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all<Color>(Colors.pink),
-              shape: MaterialStateProperty.all<OutlinedBorder>(
-                const RoundedRectangleBorder(borderRadius: BorderRadius.zero),
-              ),
-            ),
-            onPressed: () async {
-              try {
-                throw Exception(AppStrings.error);
-              } catch (error, stackTrace) {
-                await takeScreenshotAndUpload(errorMessage: error.toString(), stackTrace: stackTrace);
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(
-                    duration: Duration(seconds: 5),
-                    backgroundColor: Colors.red,
-                    content: Text("Something went wrong!"),
-                  ),
-                );
-              }
-            },
-            child: const Text(
-              "BUY NOW",
+      child: ModalProgressHUD(
+        inAsyncCall: loading,
+        child: Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            backgroundColor: Colors.pink,
+            title: const Text(
+              "SHOP INFY",
               style: TextStyle(
                 color: Colors.white,
-                fontSize: 24,
                 fontWeight: FontWeight.w700,
               ),
             ),
           ),
-        ),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 12),
+          drawer: Drawer(
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                Image.network(
-                  "https://media-ik.croma.com/prod/https://media.croma.com/image/upload/v1708671939/Croma%20Assets/Communication/Mobiles/Images/300679_0_ywgnrd.png",
-                ),
-                const SizedBox(height: 32),
-                const Text(
-                  "Apple iPhone 15 (128GB, Pink)",
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.w700,
+                const UserAccountsDrawerHeader(
+                  accountName: Text("Hello, Priyanka"),
+                  accountEmail: Text("priyanka@gmail.com"),
+                  currentAccountPicture: CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        "https://media.licdn.com/dms/image/D5635AQE2mVueX0wykA/profile-framedphoto-shrink_800_800/0/1713593490025?e=1716390000&v=beta&t=Rm9vFRZK4FGi9X56MOqlAlKK5e12isIRd-zvPeQ9zPk"),
                   ),
                 ),
-                const SizedBox(height: 16),
-                Row(
-                  children: [
-                    FilterChip(label: const Text("128 GB"), onSelected: (_) {}),
-                    const SizedBox(
-                      width: 12,
-                    ),
-                    FilterChip(label: const Text("256 GB"), onSelected: (_) {}),
-                    const SizedBox(
-                      width: 12,
-                    ),
-                    FilterChip(label: const Text("1 TB"), onSelected: (_) {})
-                  ],
-                ),
-                const SizedBox(height: 16),
-                const Text(
-                  "The iPhone 15 marks a significant leap in Apple's flagship smartphone lineup, offering cutting-edge technology and sleek design. It features a stunning 6.1-inch Super Retina XDR display with ProMotion technology for smoother visuals and a more responsive touch experience. Powered by the new A17 Bionic chip, it delivers unprecedented speed and efficiency, supporting advanced AI capabilities and enhanced graphics performance. The camera system has been revamped with a 48MP main sensor, allowing for ultra-high-resolution photos and 8K video recording. Additionally, the iPhone 15 introduces a longer-lasting battery, 5G connectivity, and iOS 17, which brings a host of new features and improvements. With its durable Ceramic Shield front cover and IP68 water resistance, the iPhone 15 is built to withstand the rigors of daily use while providing an exceptional user experience.",
-                ),
-                const SizedBox(height: 16),
+                ListTile(
+                  onTap: () {
+                    Navigator.of(context).push(
+                      CupertinoPageRoute(
+                        builder: (context) => const SimulationScreen(),
+                      ),
+                    );
+                  },
+                  leading: const Icon(Icons.accessibility),
+                  title: const Text(
+                    "Simulation",
+                  ),
+                )
               ],
+            ),
+          ),
+          bottomNavigationBar: SizedBox(
+            height: 66,
+            child: OutlinedButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.pink),
+                shape: MaterialStateProperty.all<OutlinedBorder>(
+                  const RoundedRectangleBorder(borderRadius: BorderRadius.zero),
+                ),
+              ),
+              onPressed: () async {
+                setState(() {
+                  loading = true;
+                });
+                try {
+                  throw Exception(AppStrings.error);
+                } catch (error, stackTrace) {
+                  await takeScreenshotAndUpload(errorMessage: error.toString(), stackTrace: stackTrace);
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      duration: Duration(seconds: 5),
+                      backgroundColor: Colors.red,
+                      content: Text("Something went wrong!"),
+                    ),
+                  );
+                }
+                setState(() {
+                  loading = false;
+                });
+              },
+              child: const Text(
+                "BUY NOW",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ),
+          ),
+          body: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Image.network(
+                    "https://media-ik.croma.com/prod/https://media.croma.com/image/upload/v1708671939/Croma%20Assets/Communication/Mobiles/Images/300679_0_ywgnrd.png",
+                  ),
+                  const SizedBox(height: 32),
+                  const Text(
+                    "Apple iPhone 15 (128GB, Pink)",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  Row(
+                    children: [
+                      FilterChip(label: const Text("128 GB"), onSelected: (_) {}),
+                      const SizedBox(
+                        width: 12,
+                      ),
+                      FilterChip(label: const Text("256 GB"), onSelected: (_) {}),
+                      const SizedBox(
+                        width: 12,
+                      ),
+                      FilterChip(label: const Text("1 TB"), onSelected: (_) {})
+                    ],
+                  ),
+                  const SizedBox(height: 16),
+                  const Text(
+                    "The iPhone 15 marks a significant leap in Apple's flagship smartphone lineup, offering cutting-edge technology and sleek design. It features a stunning 6.1-inch Super Retina XDR display with ProMotion technology for smoother visuals and a more responsive touch experience. Powered by the new A17 Bionic chip, it delivers unprecedented speed and efficiency, supporting advanced AI capabilities and enhanced graphics performance. The camera system has been revamped with a 48MP main sensor, allowing for ultra-high-resolution photos and 8K video recording. Additionally, the iPhone 15 introduces a longer-lasting battery, 5G connectivity, and iOS 17, which brings a host of new features and improvements. With its durable Ceramic Shield front cover and IP68 water resistance, the iPhone 15 is built to withstand the rigors of daily use while providing an exceptional user experience.",
+                  ),
+                  const SizedBox(height: 16),
+                ],
+              ),
             ),
           ),
         ),
